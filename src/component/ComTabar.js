5616b4a9a67ddd07a2cc6c631bd5f9fe7e822707
Alan <langwdalan@gmail.com>
2018-11-08 17:20:23 +0800

    删除不用的依赖

--- src/component/ComTabar.js
+++ src/component/ComTabar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import style from './ComTabar.css'
 
 import { TabBar } from 'antd-mobile';
