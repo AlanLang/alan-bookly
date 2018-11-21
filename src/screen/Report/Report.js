202482d5667f3c73aa010037df891565514ded70
Alan <wenda.lang@wimisoft.com>
2018-11-21 11:14:08 +0800

    添加图表

--- src/screen/Report/Report.js
+++ src/screen/Report/Report.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
 import ComTitle from '../../component/ComTitle'
+import F2 from '@antv/f2'
 import style from './Report.css'
 class Report extends Component {
   constructor () {
     super()
+    this.state = {
+      data:[{
+        day: '15',
+        sales: 52
+      }, {
+        day: '16',
+        sales: 61
+      }, {
+        day: '17',
+        sales: 145
+      }, {
+        day: '18',
+        sales: 48
+      }, {
+        day: '19',
+        sales: 38
+      }, {
+        day: '20',
+        sales: 38
+      }, {
+        day: '21',
+        sales: 38
+      }]
+    }
+  }
+  componentDidMount(){
+    const chart = new F2.Chart({
+      id: 'weekNode',
+      pixelRatio: window.devicePixelRatio,
+      appendPadding:[12,0,0,0]
+    });
+
+    chart.source(this.state.data, {
+      sales: {
+        tickCount: 5
+      }
+    });
+    chart.tooltip({
+      showItemMarker: false,
+      onShow: function onShow(ev) {
+        let items = ev.items;
+        items[0].name = null;
+        items[0].name = items[0].title;
+        items[0].value = items[0].value+"页";
+      }
+    });
+    chart.interval().position('day*sales');
+    chart.render();
   }
   render() {
     return (
@@ -14,6 +63,10 @@ class Report extends Component {
         </div>
         <div className={style.total}>
           累计阅读 2天13小时45秒
+        </div>
+        <div className={style.week}>
+          一周阅读数据
+          <canvas className={style.weekReport} id="weekNode"></canvas>
         </div>
       </div>
     );
