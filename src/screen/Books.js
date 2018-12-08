dd62c9904b96ab0e43114957084d5f954b5017c2
Alan <langwdalan@gmail.com>
2018-12-08 15:14:02 +0800

    :arrow_up:  alanui-mobile升级到16.0.0

--- src/screen/Books.js
+++ src/screen/Books.js
@@ -4,6 +4,7 @@ import Book from '../component/Book'
 import style from './index.css'
 import BookModel from '../models/BookModel'
 import storage from '../utils/StorageUtil'
+import {FabButton,Icon} from 'alanui-mobile';
 
 class Books extends Component {
   constructor () {
@@ -37,12 +38,13 @@ class Books extends Component {
     }
   }
   handelBookClick(re){
-    if(re._id === "addBook"){
-      this.props.history.push('/add')
-    }else{
-      this.props.history.push(`/read/${re._id}`)
-    }
+    this.props.history.push(`/read/${re._id}`)
   }
+
+  handleAddBook=()=>{
+    this.props.history.push('/add')
+  }
+
   renderBook(){
     const width = document.documentElement.clientWidth/3 - 15;
     const height = width * 1.35
@@ -66,16 +68,12 @@ class Books extends Component {
       <div>
         <ComTitle type="none" menu={this.state.menu} onSelect={this.handleMenuClick.bind(this)}>所有书籍</ComTitle>
         <div className={style.bookGroup}>
-          <Book onBookClick={this.handelBookClick.bind(this)} key="add" 
-            book={
-              {img:'http://dada-image-bed.oss-cn-shenzhen.aliyuncs.com/18-11-18/2844711.jpg',
-              _id:'addBook',
-              width:width,
-              height:height}
-            }
-          ></Book>
           {this.renderBook()}
         </div>
+        <FabButton reverse={true} style={{bottom:70}} icon={(<Icon icon="add"/>)}>
+            <Icon icon="add" onClick={this.handleAddBook}/>
+            <Icon icon="face"/>
+        </FabButton>
       </div>
     );
   }
